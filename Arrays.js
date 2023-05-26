9b5b00799fc5fa2d55bfa099550cc8717db635d0
TChester <trevin.chester@gmail.com>
2023-05-26 09:18:17 -0400

    polishing syntax and descriptions

--- Arrays.js
+++ Arrays.js
@@ -11,12 +11,15 @@ let input = document.getElementById("input");
 let pushButton = document.getElementById("pushButton");
 let popButton = document.getElementById("popButton");
 let length = document.getElementById("length");
+
 let arr = [];
+
 content.innerText=arr;
 length.innerText=0;
 
 pushButton.onclick = updatePush;
 popButton.onclick = updatePop;
+
 
 /**
  * on button click, grab the input value, have the arrPush function push the value to arr, and update the site
@@ -27,6 +30,8 @@ function updatePush(){
     content.innerText=arr;
     length.innerText=arrLength();
 }
+
+
 /**
  * on button click, have the arrPush function pop a value from arr, and update the site
  * no need to change this function
@@ -36,19 +41,31 @@ function updatePop(){
     content.innerText=arr;
     length.innerText=arrLength();
 }
+
+
 /**
  * TODO: push a new value to the arr array.
  * @param item a new value
  */
 function arrPush(item){
+    // code here
 }
+
+
 /**
  * TODO: pop a value from the arr array.
  */
 function arrPop(){
+    // code here
 }
+
+
 /**
  * TODO: return the length of arr
  */
 function arrLength(){
+    // code here
 }
+
+
+/* OPTIONAL PRACTICE: If you want more practice later, try pushing various datatypes to arrays. */
